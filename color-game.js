c73afe1ba56cb4660adcda15ae0560f83b845fe8
jo <joannamakary@hotmail.com>
2018-05-06 18:44:59 -0400

    Add files via upload

---
+++ color-game.js
@@ -0,0 +1,101 @@
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+var resultDisplay = document.querySelector("#result");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+init();
+
+function init(){
+	// Mode Button event listeners		
+	for(var i = 0; i < modeButtons.length; i++){
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			if(this.textContent === "Easy"){
+				numSquares = 3;
+			} else {
+				numSquares = 6;
+			}
+			reset();
+		});
+	}
+
+	for(var i = 0; i < squares.length; i++){
+		squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			resultDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
+			changeColors(clickedColor);
+		} else {
+			this.style.backgroundColor = "#232323";
+			resultDisplay.textContent = "Try Again";
+		}
+	});
+	}
+	reset();
+}
+
+function reset(){
+	// new colors
+	colors = generateRandomColors(numSquares);
+	// new random color from array
+	pickedColor = pickColor();
+	// change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors"
+	resultDisplay.textContent = "";
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+	h1.style.backgroundColor = "9E9FE8";
+}
+
+resetButton.addEventListener("click", function(){
+	reset();
+})
+
+colorDisplay.textContent = pickedColor;
+
+// when correct color is chosen, change all squares to that color
+function changeColors(color) {
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+	h1.style.backgroundColor = color;
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr = [];
+	for(var i = 0; i < num; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
